c1d7121ce3dcd2b496f4a79b350059aa3b9a1d14
ladbrone <bru.catalan@duocuc.cl>
2025-11-02 12:59:31 -0300

    chore(init): proyecto Evaluacion2_PetsOnline con estructura MVVM y dependencias base

---
+++ app/src/main/java/com/example/evaluacion2_petsonline/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.evaluacion2_petsonline
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.evaluacion2_petsonline.ui.theme.Evaluacion2_PetsOnlineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Evaluacion2_PetsOnlineTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Evaluacion2_PetsOnlineTheme {
+        Greeting("Android")
+    }
+}
